5b1f95ed15402c6faf18c7cbee93941b08220b95
aaalpineee <aaalpineee@outlook.com>
2025-04-13 17:12:27 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while true; do
     echo "请选择一个选项："
-    select option in "显示设置" "启动服务" "停止服务" "查看状态" "打开开机自启动" "关闭开机自启动" "退出"
+    select option in "显示设置" "启动服务" "停止服务" "查看状态" "打开开机自启动" "关闭开机自启动" "卸载" "退出"
     do
         case $REPLY in
             1) /usr/local/x-ui/x-ui setting -show; break ;;
@@ -10,7 +10,8 @@ while true; do
             4) rc-service x-ui status; break ;;
             5) rc-update add x-ui; break ;;
             6) rc-update del x-ui; break ;;
-            7) echo "已退出。"; exit 0 ;;
+            7) rc-service x-ui stop; rc-update del x-ui; rm -rf /usr/local/x-ui /usr/bin/x-ui /etc/init.d/x-ui; break ;;
+            8) echo "已退出。"; exit 0 ;;
             *) echo "无效的选项，请重新选择。" ;;
         esac
     done
